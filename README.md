3480a2a8e6e668f77eb3ca56800813086ee22a21
Mike Kelly <mikeakelly@hotmail.com>
2018-08-13 18:13:39 +0100

    Edit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Chico
+Chico is a mock server to:
+* Receive event publications as an InterSystems HealthShare EMS
+* Receive event publications as MESH
+* Receive pointers as NRLS
+
+## Installation
+This is a Node application, therefore you need to first install [Node](https://nodejs.org/en/) on your computer.
+
+Next clone or download the application code into a directory.
+
+From the command-line move to the directory and run:
+
+`npm install`
+
+## Usage
+Start the server by:
+
+**`node chico.js`**
+
+You will then see:
+
+`Chico server is up on port 80`
+
+Chico will continue to listen for incoming HTTP requests until you terminate it by `CTRL C`.
+
+## Configuration
+Configuration of Chico is defined by values in the configuration file:
+
+`config/chico.json`
+
+The following configuration items can be set:
+
+| Item | Description | Data type | Default |
+|------|-------------|-----------|---------|
+| `port` | The port number on which Chico will listen for incoming HTTP requests. | Number | `80` |
+| 'healthshare.showMethod' | Show HTTP request method. | boolean | `true` |
+| `healthshare.showURL` | Show HTTP request URL. | boolean | `true` |
+| `healthshare.showHeaders` | Show HTTP request headers. | boolean | `true` |
+|  `healthshare.showBody` | Show HTTP body. | boolean | `true` | 
--- chico.js
+++ chico.js
@@ -1,19 +1,65 @@
 // Mock web server
 
 const express = require('express');
-const port = 80;
+const bodyParser = require('body-parser');
+const colors = require('colors');
+const jsonSchema = require('jsonschema');
+const fs = require('fs');
+const path = require('path');
 
-var server = express();
+//const PORT = 80;
+const CONFIG_FOLDER = 'config';
+const CONFIG_FILENAME = 'chico.json';
+const FILE_ENCODING = 'utf8';
+
+const server = express();
+server.use(bodyParser.text({ type: ['application/json+fhir', 'application/xml+fhir'] }));
+const config = loadConfig();
 
 // Listen for publications sent to HealthShare EMS
-server.put('/Bundle/*', (req, res) => {
-  console.log('***** HealthShare Publication *****')
-  console.log(req.headers);
-  console.log(req.body);
-  res.send('Hello');
+server.put('/ems/fhir/Bundle/*', (req, res) => {
+  console.log(' ');
+  console.log('** HealthShare Publication Received **'.yellow.inverse);
+  if (config.healthshare.showMethod) {
+    console.log('HTTP Method:'.yellow);
+    console.log(req.method);
+  }
+  if (config.healthshare.showURL) {
+    console.log('HTTP URL:'.yellow);
+    console.log(req.protocol + '://' + req.hostname + req.originalUrl);
+  }
+  if (config.healthshare.showHeaders) {
+    console.log('HTTP Headers:'.yellow);
+    console.log(req.headers);
+  }
+  if (config.healthshare.showBody) {
+    console.log('HTTP Body:'.yellow);
+    console.log(req.body);
+  }
+  res.send('Thank you');
   res.status(200).end();
 });
 
-server.listen(port, () => {
-  console.log('Server is up on port ' + port);
+server.listen(config.port, () => {
+  console.log('Chico server is up on port ' + config.port);
 });
+
+// Load config
+function loadConfig()
+{
+  // Set config to default values
+  let config = { port: 80, healthshare: { showMethod: true, showURL: true, showHeaders: true, showBody: true},
+                mesh: { },
+                nrls: {}
+              };
+
+  try {
+    // read the config file
+    config = JSON.parse(fs.readFileSync(path.join(__dirname, './', CONFIG_FOLDER, CONFIG_FILENAME), FILE_ENCODING));
+  } catch(e) {
+    // If there is a problem just use the defaults
+    console.log('Error reading config file, therefore default values will be used.');
+  }
+
+  return config;
+}
